b7f7a1864f3653afe7de164ce06b6f424e383015
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-24 11:32:49 -0600

    added image, texts and score

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 
 class BasicPage extends StatelessWidget {
+  final titleStyle = TextStyle(
+      fontSize: 20.0, color: Colors.black54, fontWeight: FontWeight.bold);
+  final subtitleStyle = TextStyle(fontSize: 18.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('data'),
+      body: Column(
+        children: <Widget>[
+          Image(
+            image: NetworkImage('https://bit.ly/2N9SoNr'),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Santorini', style: titleStyle),
+                      SizedBox(height: 5.0),
+                      Text('Greece', style: subtitleStyle),
+                    ],
+                  ),
+                ),
+                Icon(Icons.star, color: Colors.lightBlue, size: 30.0),
+                Text('41', style: TextStyle(fontSize: 20.0)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
