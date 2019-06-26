459e9fb31975e9f68692b98f88723e573796e69d
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-25 20:36:02 -0600

    added title text and some theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:designs/src/pages/basic_page.dart';
 import 'package:designs/src/pages/buttons_page.dart';
 import 'package:designs/src/pages/scroll_page.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.white,
+    ));
+
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
--- lib/src/pages/buttons_page.dart
+++ lib/src/pages/buttons_page.dart
@@ -1,14 +1,32 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
 class ButtonsPage extends StatelessWidget {
+  final titleStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  final subtitleStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 18.0,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
           _background(),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _titles(),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -53,6 +71,25 @@ class ButtonsPage extends StatelessWidget {
           left: -40.0,
         ),
       ],
+    );
+  }
+
+  Widget _titles() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Classify Transactions', style: titleStyle),
+            SizedBox(height: 10.0),
+            Text(
+              'Classified transactions into a particular category',
+              style: subtitleStyle,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
